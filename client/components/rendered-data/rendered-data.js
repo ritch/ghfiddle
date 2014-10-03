b38f9373b4fc546d72751019c802c7414a2a5a9a
Jeff Cross <middlefloor@gmail.com>
2014-10-02 23:30:51 -0700

    fix(renderedData): react to script changes

--- client/components/rendered-data/rendered-data.js
+++ client/components/rendered-data/rendered-data.js
@@ -7,24 +7,25 @@ angular.module('ghaRenderedData', [])
     };
   })
   .controller('RenderedDataController', ['$scope', '$compile', '$sce',  'templateDataService', function($scope, $compile, $sce, templateDataService) {
+    var compiledTemplate;
     $scope.data = templateDataService._data;
+    $scope.$sce = $sce;
+
+    function applyTemplateToDom () {
+      console.log('applyTemplateToDom');
+      $scope.$evalAsync(function() {
+        $scope.renderedHTML = $sce.trustAsHtml(compiledTemplate($scope).html());
+      });
+    }
+
+    $scope.$watch('data', applyTemplateToDom, true);
 
     $scope.$watch('templateSource', function(templateSource) {
       if(!templateSource) return;
 
       templateSource = ['<div>', templateSource, '</div>'].join('');
 
-      var el = $compile(templateSource)($scope);
-      el = el[0];
-      console.log('el', el);
-      if(!el) return;
-
-
-
-      $scope.$evalAsync(function() {
-        var html = el.innerHTML;
-        $scope.renderedHTML = $sce.trustAsHtml(html);
-        console.log('$scope.renderedHTML', $scope.renderedHTML.$$unwrapTrustedValue());
-      });
+      compiledTemplate = $compile(templateSource);
+      applyTemplateToDom();
     });
   }]);
